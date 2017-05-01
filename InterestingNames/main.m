293f394023e09309888066e20f55988db55d47f3
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2017-05-01 11:58:39 -0400

    Initial Commit

---
+++ InterestingNames/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InterestingNames
+//
+//  Created by Katherine Owens on 5/1/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
